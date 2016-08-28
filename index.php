79bec0a1105fcc7d2ade2d2b33af1944d53609d9
Artem Iksanov <iksanov@rekuz.ru>
2016-08-29 01:18:36 +0300

    #desc Add new description

--- index.php
+++ index.php
@@ -1,22 +1,12 @@
 <?php
 /**
- * CodeIgniter
+ * PassIgniter
  *
- * An open source application development framework for PHP
+ * An open source multi-user password management application fully based on CodeIgniter framework for PHP
  *
- * This content is released under the MIT License (MIT)
+ * This content is released under the General Public License (GNU) (see LICENSE.txt for more information)
  *
- * Copyright (c) 2014 - 2016, British Columbia Institute of Technology
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
+ * Copyright (c) 2016, Artem Iksanov
  *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
@@ -26,13 +16,12 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  *
- * @package	CodeIgniter
- * @author	EllisLab Dev Team
- * @copyright	Copyright (c) 2008 - 2014, EllisLab, Inc. (https://ellislab.com/)
- * @copyright	Copyright (c) 2014 - 2016, British Columbia Institute of Technology (http://bcit.ca/)
- * @license	http://opensource.org/licenses/MIT	MIT License
- * @link	https://codeigniter.com
- * @since	Version 1.0.0
+ * @package	PassIgniter
+ * @author	Artem Iksanov
+ * @copyright	Copyright (c) 2016, Artem Iksanov (http://iksanov.rekuz.ru/)
+ * @license	https://www.gnu.org/licenses/gpl-3.0.html License
+ * @link	https://github.com/bender-nvkz/PassIgniter
+ * @since	Version 0.0.1-alpha
  * @filesource
  */
 
